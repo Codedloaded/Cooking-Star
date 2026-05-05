dd311311492197caacae23fc4e9309b6e797d23a
mazenn693 <mazensheriff888@gmail.com>
2026-05-05 22:29:54 +0200

    mazen

--- Backend_cookingstar/recipes/models.py
+++ Backend_cookingstar/recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    image = models.ImageField(upload_to='recipes/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- Backend_cookingstar/recipes/views.py
+++ Backend_cookingstar/recipes/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Recipe
+from .forms import RecipeForm
 
-# Create your views here.
+
+def create_recipe(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('create_recipe')  # change if you have list page
+    else:
+        form = RecipeForm()
+    
+    return render(request, 'recipes/create.html', {'form': form})
+
+
+def edit_recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('create_recipe')  # or your main page
+    else:
+        form = RecipeForm(instance=recipe)
+
+    return render(request, 'recipes/edit.html', {'form': form})

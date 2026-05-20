1467b20e0d2fce617aee5e1ef430a87649eead36
Jana Elhenawy <janaelhenawy06@gmail.com>
2026-05-20 20:13:36 +0300

    Media

--- Backend_cookingstar/backend/urls.py
+++ Backend_cookingstar/backend/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
 
 # ── Serve media uploads in development ────────────────────────────────────────
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
